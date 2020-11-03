93cb7446d318810310543ba4eb155a4ebf977b79
Nyxara <mvd7241@rit.edu>
2020-11-03 12:37:44 -0500

    step 27 now

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
+const csrf = require('csurf');
 const redis = require('redis');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
@@ -54,6 +55,7 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -76,6 +78,17 @@ app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+
+//csrf must come AFTER app.use(cookieParser());
+//& app.use(session({ . . . }) );
+//but still be before router(app);
+app.use(csrf());
+app.use((err,req,res,next) => {
+    if(err.code !== 'EBADCSRFTOKEN') return next(err);
+    
+    console.log('Missing CSRF token');
+    return false;
+});
 
 router(app);
 
